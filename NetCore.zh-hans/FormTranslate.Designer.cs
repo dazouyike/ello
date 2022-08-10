db862ef30496009b07721e025461fd8437e4f477
dazouyike <110949433+dazouyike@users.noreply.github.com>
2022-08-10 09:15:06 +0800

    Initial commit

---
+++ NetCore.zh-hans/FormTranslate.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace NetCore.zh_hans
+{
+    partial class FormTranslate
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_appId = new System.Windows.Forms.TextBox();
+            this.textBox_secretKey = new System.Windows.Forms.TextBox();
+            this.button_Import = new System.Windows.Forms.Button();
+            this.progressBar_Translate = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "百度appId:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 136);
+            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 35);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "secretKey:";
+            // 
+            // textBox_appId
+            // 
+            this.textBox_appId.Location = new System.Drawing.Point(267, 45);
+            this.textBox_appId.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.textBox_appId.Name = "textBox_appId";
+            this.textBox_appId.Size = new System.Drawing.Size(530, 42);
+            this.textBox_appId.TabIndex = 1;
+            this.textBox_appId.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_secretKey
+            // 
+            this.textBox_secretKey.Location = new System.Drawing.Point(267, 134);
+            this.textBox_secretKey.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.textBox_secretKey.Name = "textBox_secretKey";
+            this.textBox_secretKey.Size = new System.Drawing.Size(530, 42);
+            this.textBox_secretKey.TabIndex = 1;
+            this.textBox_secretKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button_Import
+            // 
+            this.button_Import.Location = new System.Drawing.Point(103, 305);
+            this.button_Import.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.button_Import.Name = "button_Import";
+            this.button_Import.Size = new System.Drawing.Size(709, 191);
+            this.button_Import.TabIndex = 2;
+            this.button_Import.Text = "批量导入Xml文件 | 开始翻译";
+            this.button_Import.UseVisualStyleBackColor = true;
+            this.button_Import.Click += new System.EventHandler(this.button_Import_Click);
+            // 
+            // progressBar_Translate
+            // 
+            this.progressBar_Translate.Location = new System.Drawing.Point(103, 226);
+            this.progressBar_Translate.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.progressBar_Translate.Name = "progressBar_Translate";
+            this.progressBar_Translate.Size = new System.Drawing.Size(699, 47);
+            this.progressBar_Translate.TabIndex = 3;
+            // 
+            // FormTranslate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 521);
+            this.Controls.Add(this.progressBar_Translate);
+            this.Controls.Add(this.button_Import);
+            this.Controls.Add(this.textBox_secretKey);
+            this.Controls.Add(this.textBox_appId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(924, 600);
+            this.MinimumSize = new System.Drawing.Size(924, 600);
+            this.Name = "FormTranslate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VS.NetCore“注释摘要\"中文翻译 zh-hans";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormTranslate_FormClosing);
+            this.Load += new System.EventHandler(this.FormTranslate_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_appId;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_secretKey;
+        private System.Windows.Forms.Button button_in;
+        private System.Windows.Forms.Button button_ImportFile;
+        private System.Windows.Forms.Button button_Import;
+        private System.Windows.Forms.Button button_ImportXML;
+        private System.Windows.Forms.ProgressBar progressBar_;
+        private System.Windows.Forms.ProgressBar progressBar_t;
+        private System.Windows.Forms.ProgressBar progressBar_Translate;
+    }
+}
+
---
+++ NetCore.zh-hans/Translate.cs
@@ -0,0 +1,155 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NetCore.zh_hans
+{
+    internal class Translate
+    {
+        // 计算MD5值
+        public static string EncryptString(string str)
+        {
+            var md5 = MD5.Create();
+            // 将字符串转换成字节数组
+            var byteOld = Encoding.UTF8.GetBytes(str);
+            // 调用加密方法
+            var byteNew = md5.ComputeHash(byteOld);
+            // 将加密结果转换为字符串
+            var sb = new StringBuilder();
+            foreach (var b in byteNew)
+            {
+                // 将字节转换成16进制表示的字符串，
+                sb.Append(b.ToString("x2"));
+            }
+            // 返回加密的字符串
+            return sb.ToString();
+        }
+
+        private static readonly HttpClient _httpClient = new HttpClient(new SocketsHttpHandler());
+        /// <summary>
+        /// 调用百度翻译API并返回结果
+        /// </summary>
+        /// <param name="str">要翻译的英文字符</param>
+        /// <param name="inputAppId">百度翻译接口|AppId </param>
+        /// <param name="inputSecretKey">百度翻译接口|SecretKey </param>
+        /// <returns>返回的翻译结果</returns>
+        public static async Task<string> TranslateText(string str, string inputAppId, string inputSecretKey)
+        {
+            // 源语言
+            var from = "en";
+            // 目标语言
+            var to = "zh";
+            // 改成您的APP ID
+            var appId = inputAppId;
+            var rd = new Random();
+            var salt = rd.Next(100000).ToString();
+            // 改成您的密钥
+            var secretKey = inputSecretKey;
+            var sign = EncryptString(appId + str + salt + secretKey);
+            var url = "http://api.fanyi.baidu.com/api/trans/vip/translate?";
+            url += "q=" + HttpUtility.UrlEncode(str);
+            url += "&from=" + from;
+            url += "&to=" + to;
+            url += "&appid=" + appId;
+            url += "&salt=" + salt;
+            url += "&sign=" + sign;
+
+            var retString = "";
+            try
+            {
+                retString = await _httpClient.GetStringAsync(url);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"translate [{str}] error,{e.Message}");
+            }
+
+            if (string.IsNullOrWhiteSpace(retString))
+            {
+                Debug.WriteLine($"translate [{str}] error, content is null");
+                return null;
+            }
+
+            var result = JsonConvert.DeserializeObject<TranslateResult>(retString);
+
+            if (result.error_code != 0)
+            {
+                Debug.WriteLine($"translate [{str}] error, {retString}");
+                return null;
+            }
+
+            return result.trans_result.FirstOrDefault()?.dst;
+
+            //返回值做一下处理 
+            //var regex = new Regex("(?<=(" + "dst\":\"" + "))[.\\s\\S]*?(?=(" + "\"}]" + "))");//筛选标准
+            //var matchVar = regex.Matches(retString);//匹配返回的Unicode结果
+            //if (matchVar.Count <= 0)
+            //{
+            //    return null;
+            //}
+
+            //var unicodeStr = matchVar[0].Value;
+            //var resultsOkStr = UnicodeToString(unicodeStr);
+            //return resultsOkStr;
+
+        }
+
+        //https://api.fanyi.baidu.com/api/trans/product/apidoc
+        public class TranslateResult
+        {
+            public int error_code { get; set; }
+            public string from { get; set; }
+            public string to { get; set; }
+            public Trans_Result[] trans_result { get; set; }
+        }
+
+        public class Trans_Result
+        {
+            /// <summary>
+            /// 原文
+            /// </summary>
+            public string src { get; set; }
+            /// <summary>
+            /// 译文
+            /// </summary>
+            public string dst { get; set; }
+        }
+
+
+        /// <summary>
+        /// Unicode转字符串
+        /// </summary>
+        /// <param name="source">经过Unicode编码的字符串</param>
+        /// <returns>正常字符串</returns>
+        public static string UnicodeToString(string source)
+        {
+            return new Regex(@"\\u([0-9A-F]{4})", RegexOptions.IgnoreCase | RegexOptions.Compiled).Replace(
+                source, x => string.Empty + Convert.ToChar(Convert.ToUInt16(x.Result("$1"), 16)));
+        }
+
+        /// <summary>
+        /// 字符串转Unicode
+        /// </summary>
+        /// <param name="source">源字符串</param>
+        /// <returns>Unicode编码后的字符串</returns>
+        public static string String2Unicode(string source)
+        {
+            var bytes = Encoding.Unicode.GetBytes(source);
+            var stringBuilder = new StringBuilder();
+            for (var i = 0; i < bytes.Length; i += 2)
+            {
+                stringBuilder.AppendFormat("\\u{0}{1}", bytes[i + 1].ToString("x").PadLeft(2, '0'), bytes[i].ToString("x").PadLeft(2, '0'));
+            }
+            return stringBuilder.ToString();
+        }
+
+
+    }
+}
